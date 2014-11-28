0bfcbb2337ec331d2955e569e4a13a0e539c0a8c
wduntak <wduntak@gmail.com>
2014-11-28 14:56:06 -0500

    Completed Exercise 3

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -6,10 +6,17 @@
 //Exercise 2 ----
 
 
-var result = 0;
+// var result = 0;
 
-for (var i = 2; i < (process.argv).length; i++) {
-result += Number(process.argv[i]);
-}
+// for (var i = 2; i < (process.argv).length; i++) {
+// result += Number(process.argv[i]);
+// }
 
-console.log(result);
+// console.log(result);
+
+//Exercise 3 -----
+
+// var fs = require('fs');
+// var contents = fs.readFileSync(process.argv[2]);
+// var lines = contents.toString().split('\n').length - 1;
+// console.log(lines);
