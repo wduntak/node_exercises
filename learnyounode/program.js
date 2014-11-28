26fff27d0298604c98d163a760b8b04c8e044000
wduntak <wduntak@gmail.com>
2014-11-28 15:39:28 -0500

    Completed Exercise 4

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -20,3 +20,11 @@
 // var contents = fs.readFileSync(process.argv[2]);
 // var lines = contents.toString().split('\n').length - 1;
 // console.log(lines);
+
+//Exercise 4 ----
+
+var fs = require('fs');
+var contents = fs.readFile(process.argv[2], function(err, contents){
+	var lines = contents.toString().split('\n').length - 1
+	console.log(lines)
+});
