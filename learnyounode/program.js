372e6424b7d85e123f2a68140332486b8d66d0e9
wduntak <wduntak@gmail.com>
2014-11-28 14:32:06 -0500

    Completed Exercise 2

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,1 +1,15 @@
-console.log("HELLO WORLD");
+//Exerise 1 ----
+
+//console.log("HELLO WORLD");
+
+
+//Exercise 2 ----
+
+
+var result = 0;
+
+for (var i = 2; i < (process.argv).length; i++) {
+result += Number(process.argv[i]);
+}
+
+console.log(result);
