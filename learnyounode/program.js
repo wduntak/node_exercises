6aedf8bf336729c2838a92f13951fe4712baeb4b
wduntak <wduntak@gmail.com>
2014-11-28 23:35:49 -0500

    Completed Exercise 5

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -23,8 +23,19 @@
 
 //Exercise 4 ----
 
+// var fs = require('fs');
+// var contents = fs.readFile(process.argv[2], function(err, contents){
+// 	var lines = contents.toString().split('\n').length - 1
+// 	console.log(lines)
+// });
+
+//Exercise 5 ----
+
 var fs = require('fs');
-var contents = fs.readFile(process.argv[2], function(err, contents){
-	var lines = contents.toString().split('\n').length - 1
-	console.log(lines)
+var path = require('path');
+	fs.readdir(process.argv[2], function (err, list){
+	list.forEach(function(file){
+		if (path.extname(file) === '.' + process.argv[3])
+			console.log(file);
+	});
 });
